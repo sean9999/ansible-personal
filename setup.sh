a5725a45f326627909146f3713dffc42e40a276e
David Hoffman <dhoffman@Davids-MacBook-Pro.local>
2015-03-26 20:35:14 -0400

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo easy_install pip
+sudo pip install ansible
+mkdir ~/projects
+cd ~/projects
+git clone https://david_hoffman_@bitbucket.org/david_hoffman_/ansible-personal.git
+cd ansible-personal
+ansible-playbook -i localhost, main.yml
