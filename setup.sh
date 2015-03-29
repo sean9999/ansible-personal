e45d5b3e8d2ab02088521f7f7ee6d155195eb8db
David Hoffman <dhoffman34@icloud.com>
2015-03-29 18:41:59 -0400

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ mkdir ~/projects
 cd ~/projects
 git clone https://david_hoffman_@bitbucket.org/david_hoffman_/ansible-personal.git
 cd ansible-personal
-ansible-playbook -i localhost, main.yml
+ansible-playbook -i localhost, main.yml --ask-sudo-pass
