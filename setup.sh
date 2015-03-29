4c0a0dc37eb78ec0e53fa7127d379903c95af384
David Hoffman <dhoffman34@icloud.com>
2015-03-29 19:07:33 -0400

    updated install steps

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ sudo easy_install pip
 sudo pip install ansible
 mkdir ~/projects
 cd ~/projects
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
 git clone https://david_hoffman_@bitbucket.org/david_hoffman_/ansible-personal.git
 cd ansible-personal
 ansible-playbook -i localhost, main.yml --ask-sudo-pass
+
